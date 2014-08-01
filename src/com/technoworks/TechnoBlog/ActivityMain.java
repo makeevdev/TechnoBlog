535fbe2acb00d8b21c7d651b61a7208da2ab4f95
makeevdev <makeevvolodja@gmail.com>
2014-08-01 11:01:07 +0400

    грузит итемы, но из за кривой работы с потоками стреляет себе в ногу

--- src/com/technoworks/TechnoBlog/ActivityMain.java
+++ src/com/technoworks/TechnoBlog/ActivityMain.java
@@ -1,6 +1,7 @@
 package com.technoworks.TechnoBlog;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -11,51 +12,68 @@ import com.technoworks.TechnoBlog.view.BlogListView;
 
 public class ActivityMain extends Activity {
     public final String ACTIVITY_LOG = "ActivityMain";
-    /**
-     * Called when the activity is first created.
-     */
-
+    public boolean isDownloaded = false;
     public BlogListView blogList;
+    LinearLayout linLayoutList;
+    LayoutInflater ltInflater;
+    private TextView tvLoadingState;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        isDownloaded = false;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        final LinearLayout linLayoutList = (LinearLayout) findViewById(R.id.linLayoutList);
-        final LayoutInflater ltInflater = getLayoutInflater();
-
-        Thread network = new Thread() { // поток тянет данные с сервера, формирует лист с данными
-            @Override
-            public void run() {
-                String out = DataSender.getStringFromHTTP("/blag/api/v1.0/posts/");
-                if (out != null) {
-                    blogList = new BlogListView();
-                    blogList.setPostsData(out);
-                    Log.d(ACTIVITY_LOG, "GET completed");
-
-
-                } else
-                    Log.d(ACTIVITY_LOG, "GET failed");
-            }
-        };
-        network.start();
-
-        try {
-            network.join();
-        } catch (InterruptedException e) {
-        }
-
-
-        for (int i = 0; i < blogList.size(); i++) {
-            View itemView = ltInflater.inflate(R.layout.item, linLayoutList, false);
-            TextView tvSummary = (TextView) itemView.findViewById(R.id.tvSummary);
-            tvSummary.setText(blogList.getSummary(i));
-            TextView tvCategory = (TextView) itemView.findViewById(R.id.tvCategory);
-            tvCategory.setText(blogList.getCategories(i));
-            linLayoutList.addView(itemView);
-        }
-
+        linLayoutList = (LinearLayout) findViewById(R.id.linLayoutList);
+        ltInflater = getLayoutInflater();
+        tvLoadingState = (TextView) findViewById(R.id.tvLoadingState);
+        tvLoadingState.invalidate();
 
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (!isDownloaded) {
+            Thread network = new Thread() { // поток тянет данные с сервера, формирует лист с данными
+                @Override
+                public void run() {
+                    String out = DataSender.getStringFromHTTP("/blag/api/v1.0/posts/");
+                    if (out != null) {
+                        blogList = new BlogListView();
+                        blogList.setPostsData(out);
+                        Log.d(ACTIVITY_LOG, "GET completed");
+                        isDownloaded = true;
+
+                    } else
+                        Log.d(ACTIVITY_LOG, "GET failed");
+                }
+            };
+            network.start();
+
+
+            try {
+                network.join();
+            } catch (InterruptedException e) {
+            }
+
+
+            if (isDownloaded) {
+
+                for (int i = blogList.size() - 1; i > 0; i--) {
+                    View itemView = ltInflater.inflate(R.layout.item, linLayoutList, false);
+                    TextView tvSummary = (TextView) itemView.findViewById(R.id.tvSummary);
+                    tvSummary.setText(blogList.getSummary(i));
+                    TextView tvCategory = (TextView) itemView.findViewById(R.id.tvCategory);
+                    tvCategory.setText(blogList.getCategories(i));
+                    linLayoutList.addView(itemView);
+                    linLayoutList.removeView(tvLoadingState);
+                }
+            } else {
+                tvLoadingState.setTextColor(Color.RED);
+                tvLoadingState.setText("Loading failed");
+            }
+        }
+    }
+
 }
